96a2a809aa3b3818372a77a0d3f50e42b1809c23
RadMa <maks25022@gmail.com>
2023-09-21 20:43:31 +0500

    Program is done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+void lessThan(int maxl, string[] array)
+{
+    string newStr = String.Empty;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < maxl) { newStr = newStr + array[i] + ";"; }//";;" is a separator
+    }
+    string[] newArr = newStr.Split(';');
+    foreach (string word in newArr) { Console.Write(word + " "); }
+}
+string[] arr = { "Hello", "World", "he", "llo", "Hello World", "h" };
+lessThan(4, arr);
